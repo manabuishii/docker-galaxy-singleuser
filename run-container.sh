5dbb575efde7e6e61e83cb8443b15d93fe7e78bc
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-09-26 07:49:48 +0000

    Setup galaxy container name and hostname

--- run-container.sh
+++ run-container.sh
@@ -17,16 +17,26 @@ if [ "x$GALAXY_CONTAINER" == "x" ]
     then
         GALAXY_CONTAINER=bgruening/galaxy-stable:dev
 fi
+if [ "x$GALAXY_CONTAINER_NAME" == "x" ]
+    then
+        GALAXY_CONTAINER_NAME=galaxytest
+fi
+if [ "x$GALAXY_CONTAINER_HOSTNAME" == "x" ]
+    then
+        GALAXY_CONTAINER_HOSTNAME=galaxytest
+fi
 
 
 
-docker run -e GALAXY_USER=${GALAXY_USER} \
+docker run -d \
+           --name ${GALAXY_CONTAINER_NAME} \
+           --hostname ${GALAXY_CONTAINER_HOSTNAME} \
+           -e GALAXY_USER=${GALAXY_USER} \
            -e GALAXY_UID=${GALAXY_UID} \
            -e GALAXY_POSTGRES_UID=${GALAXY_POSTGRES_UID} \
            -p 20080:80 -p 29002:9002  -e NONUSE="condor" \
            -v $PWD/job_conf.xml.local:/etc/galaxy/job_conf.xml \
            -v $PWD/export:/export \
            -v $PWD/setup.sh:/galaxy-central/setup.sh \
-           --rm \
            ${GALAXY_CONTAINER} \
            /galaxy-central/setup.sh
